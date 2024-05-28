e6e13921386571b9d1c3a72aa8e80adb58185eaf
penjj <peng.j.1995@gmail.com>
2024-05-28 16:39:45 +0800

    refactor: replace vue sfc file to

---
+++ packages/vue/src/index.ts
@@ -0,0 +1,49 @@
+import type { PropType } from 'vue'
+import { defineComponent, h, ref, watch } from 'vue'
+import type { Ecc } from 'qrcode-tiny'
+import { createQrCode } from 'qrcode-tiny'
+
+export default defineComponent({
+  name: 'QrCodeTiny',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    ecc: {
+      type: Object as PropType<Ecc>,
+      default: undefined,
+    },
+    width: {
+      type: Number,
+      default: 200,
+    },
+    height: {
+      type: Number,
+      default: 200,
+    },
+    dark: {
+      type: String,
+      default: '#000',
+    },
+    light: {
+      type: String,
+      default: '#fff',
+    },
+  },
+  setup(props) {
+    const src = ref<string>()
+
+    watch(props, async ({ text, ecc, ...option }) => {
+      src.value = await createQrCode(text, ecc, option)
+    }, {
+      immediate: true,
+    })
+
+    return () => h('img', {
+      src: src.value,
+      width: props.width,
+      height: props.height,
+    })
+  },
+})
